db77f7d8122f9b1fbc24985d95f2c9496bec4ceb
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2020-07-22 15:26:41 -0700

    test minor change in loading time of AdminPortal by removing defer in script

--- public/js/index.js
+++ public/js/index.js
@@ -32,25 +32,6 @@ function updateTotal(total) {
   totalHtml = document.getElementById('total');
   totalHtml.innerHTML = total;
 }
-// let ordersTable = document.getElementById('orders-table');
-// let sellersTable = document.getElementById('sellers-table');
-// let customersTable = document.getElementById('customers-table');
-// function showOrders(){
-//   ordersTable.style.display = 'block';
-//   sellersTable.style.display = 'none';
-//   customersTable.style.display = 'none';
-// }
-// function showSellers(){
-//   sellersTable.style.display = 'block';
-//   ordersTable.style.display = 'none';
-//   customersTable.style.display = 'none';
-// }
-// function showCustomers(){
-//   customersTable.style.display = 'block';
-//   sellersTable.style.display = 'none';
-//   ordersTable.style.display = 'none';
-// }
-
 // Event listeners
 window.addEventListener('DOMContentLoaded', function () {
   var total = 0.0;
@@ -88,10 +69,4 @@ window.addEventListener('DOMContentLoaded', function () {
       updateTotal(total);
     });
   }
-  // let ordersBtn = document.getElementsByClassName('orders-btn')[0];
-  // ordersBtn.addEventListener('click', showOrders);
-  // let sellersBtn = document.getElementsByClassName('sellers-btn')[0];
-  // sellersBtn.addEventListener('click', showSellers);
-  // let customersBtn = document.getElementsByClassName('customers-btn')[0];
-  // customersBtn.addEventListener('click', showCustomers);
 });
