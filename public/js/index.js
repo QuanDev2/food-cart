4c170f94c2792249f008f0cca878f91c4cc37ac3
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2020-07-24 01:59:15 -0700

    finished homepage js and live search

--- public/js/index.js
+++ public/js/index.js
@@ -79,6 +79,18 @@ function insertOrderItem(dishName, price, quantity, imgUrl) {
   orderContainer.insertAdjacentHTML('beforeend', orderItemHTML);
 }
 
+function insertPostItem(dishName, price, seller, imgUrl) {
+  var postItemContext = {
+    dishName: dishName,
+    price: price,
+    seller: seller,
+    imgUrl: imgUrl
+  };
+  var postItemHTML = Handlebars.templates.dish(postItemContext);
+  var postContainer = document.getElementById('dishes');
+  postContainer.insertAdjacentHTML('beforeend', postItemHTML);
+}
+
 function updateTotal(total) {
   // remove total before inserting
 
@@ -86,8 +98,113 @@ function updateTotal(total) {
   totalHtml = document.getElementById('total');
   totalHtml.innerHTML = total;
 }
+
+// Seach feature
+let allPosts = [];
+
+/*
+ * This function clears the current search term, causing all posts to be
+ * re-inserted into the DOM.
+ */
+function clearSearchAndReinsertPosts() {
+  document.getElementById('nav-search-input').value = '';
+  doSearchUpdate();
+}
+
+/*
+ * A function that determines whether a given post matches a search query.
+ * Returns true if the post matches the query and false otherwise.
+ */
+function postMatchesSearchQuery(post, searchQuery) {
+  /*
+   * An empty query matches all posts.
+   */
+  if (!searchQuery) {
+    return true;
+  }
+  /*
+   * The search query matches the post if either the post's text or the post's
+   * author contains the search query.
+   */
+  searchQuery = searchQuery.trim().toLowerCase();
+  return post.dishName.toLowerCase().indexOf(searchQuery) >= 0;
+}
+
+/*
+ * Perform a search over over all the posts based on the search query the user
+ * entered in the navbar.  Only display posts that match the search query.
+ * Display all posts if the search query is empty.
+ */
+function doSearchUpdate() {
+  /*
+   * Grab the search query from the navbar search box.
+   */
+  var searchQuery = document.getElementById('nav-search-input').value;
+
+  /*
+   * Remove all posts from the DOM temporarily.
+   */
+  var postContainer = document.getElementById('dishes');
+  if (postContainer) {
+    while (postContainer.lastChild) {
+      postContainer.removeChild(postContainer.lastChild);
+    }
+  }
+
+  /*
+   * Loop through the collection of all posts and add posts back into the DOM
+   * if they match the current search query.
+   */
+  allPosts.forEach(function (post) {
+    if (postMatchesSearchQuery(post, searchQuery)) {
+      insertPostItem(post.dishName, post.price, post.seller, post.imgUrl);
+    }
+  });
+}
+
+/*
+ * This function parses an existing DOM element representing a single post
+ * into an object representing that post and returns that object.  The object
+ * is structured like this:
+ *
+ * {
+ *   dishName: "...",
+ *   price: "..."
+ * }
+ */
+function parsePostElem(postElem) {
+  var post = {};
+
+  var dishNameElem = postElem.querySelector('.dish-name');
+  post.dishName = dishNameElem.textContent.trim();
+  var priceElem = postElem.querySelector('.dish-price');
+  post.price = priceElem.textContent.trim();
+  var sellerElem = postElem.querySelector('.seller-name');
+  post.seller = sellerElem.textContent.trim();
+  var imgUrlElem = postElem.querySelector('img');
+  post.imgUrl = imgUrlElem.src;
+
+  return post;
+}
+
 // Event listeners
 window.addEventListener('DOMContentLoaded', function () {
+  // Remember all of the existing twits in an array that we can use for search.
+  var postElemsCollection = document.getElementsByClassName('dish');
+  for (var i = 0; i < postElemsCollection.length; i++) {
+    allPosts.push(parsePostElem(postElemsCollection[i]));
+  }
+
+  var searchButton = document.getElementById('nav-search-button');
+  if (searchButton) {
+    searchButton.addEventListener('click', doSearchUpdate);
+  }
+
+  var searchInput = document.getElementById('nav-search-input');
+  if (searchInput) {
+    searchInput.addEventListener('input', doSearchUpdate);
+  }
+
   var total = 0.0;
   var orderList = [];
 
