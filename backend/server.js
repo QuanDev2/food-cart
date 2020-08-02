5b8ca2459863d2257259dacc8d450b029fda6fd1
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2020-08-02 02:09:24 -0700

    working on inserting orderPost

--- backend/server.js
+++ backend/server.js
@@ -28,26 +28,77 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../public/css')));
 app.use(express.static(path.join(__dirname, '../public/js')));
 app.use(express.static(path.join(__dirname, '../public/src/img')));
-// serve the landing page route
+
+/***************************
+ * Serve the home page
+ */
+
 app.get('/', (req, res) => {
   let query =
-    'select dish.dishName, seller.sellerName, post.price, post.quantity from dish ' +
+    'select post.postID ,dish.dishName, seller.sellerName, post.price, post.quantity from dish ' +
     'join dishPost using (dishID) ' +
     'join post using (postID) ' +
     'join seller using (sellerID)';
 
-  mysql.pool.query(query, (err, results) => {
+  mysql.pool.query(query, (err, posts) => {
     if (err) throw err;
-    console.log(results);
-    res.render('homepage', {
-      dishes: results
+    // console.log(results);
+
+    // get all customer names
+    const customerQuery = `SELECT customerName, customerID FROM customer`;
+
+    mysql.pool.query(customerQuery, (err, customerInfo) => {
+      if (err) throw err;
+      // console.log(customerInfo);
+      res.render('homepage', {
+        dishes: posts,
+        allCustomers: customerInfo
+      });
     });
   });
 });
 
-app.post('/new-order', (req, res) => {
-  console.log(req.body.orderList);
-  res.send('OK');
+/*****************************
+ * Handle new post from home page
+ */
+app.post('/new-order', async (req, res) => {
+  let orderTotal = 0;
+
+  // sum up the total
+  req.body.orders.forEach(element => {
+    orderTotal += parseInt(element.price) * parseInt(element.quantity);
+  });
+
+  console.log(req.body.customerName);
+  const orderList = req.body.orders;
+  const insertOrderQuery =
+    `INSERT INTO customerOrder (customerID, total) ` +
+    `VALUES ((SELECT customerID FROM customer WHERE customerName = '${req.body.customerName}'), ${orderTotal})`;
+  mysql.pool.query(insertOrderQuery, async (err, result) => {
+    if (err) throw err;
+
+    // run query to get the latest orderID
+    const latestOrderIDQuery = `select orderID from customerOrder order by orderID desc limit 1`;
+
+    // insert into orderPost
+    for (let i = 0; i < orderList.length; i++) {
+      const postID = orderList[i].postID;
+      const quantity = parseInt(orderList[i].quantity);
+      const price = parseFloat(orderList[i].price);
+      const subtotal = quantity * price;
+      const insertOrderPostQuery =
+        `INSERT INTO orderPost (postID, orderID, quantity, subtotal) VALUES(` +
+        `${postID} ,(${latestOrderIDQuery}), ${quantity}, ${subtotal})`;
+      try {
+        result = await mysql.pool.query(insertOrderPostQuery);
+      } catch {
+        err => {
+          throw err;
+        };
+      }
+    }
+    res.send('OK');
+  });
 });
 
 app.post('/create-dish', (req, res) => {
@@ -81,8 +132,16 @@ app.post('/sign-up', (req, res) => {
   });
 });
 
-app.post('/manage-post', (req, res) => {
+app.post('/manage-post-delete', (req, res) => {
   const query = `DELETE FROM post WHERE postID = ${req.body.postID}`;
+  mysql.pool.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('OK');
+  });
+});
+
+app.post('/manage-post-update', (req, res) => {
+  const query = `UPDATE post SET quantity = ${req.body.quantity}, price = ${req.body.price} WHERE postID = ${req.body.postID}`;
   mysql.pool.query(query, (err, results) => {
     if (err) throw err;
     res.send('OK');
@@ -106,8 +165,8 @@ app.get('/sell-dish', (req, res) => {
 });
 
 app.post('/create-post', (req, res) => {
-  console.log('Req body:');
-  console.log(req.body);
+  // console.log('Req body:');
+  // console.log(req.body);
   const sellerID = 0;
   mysql.pool.query(
     `SELECT sellerID FROM seller WHERE sellerName = '${req.body.sellerName}'`,
@@ -118,8 +177,8 @@ app.post('/create-post', (req, res) => {
         price: req.body.price,
         quantity: req.body.quantity
       };
-      console.log('New post:');
-      console.log(newPost);
+      // console.log('New post:');
+      // console.log(newPost);
 
       mysql.pool.query('INSERT INTO post SET ?', newPost, (err, results) => {
         if (err) throw err;
@@ -127,7 +186,7 @@ app.post('/create-post', (req, res) => {
           'SELECT postID FROM post ORDER BY postID DESC LIMIT 1;',
           (err, results) => {
             if (err) throw err;
-            console.log(results);
+            // console.log(results);
             const newPostID = results[0].postID;
 
             // query to insert dishPost
@@ -172,6 +231,8 @@ app.get('/manage-posts', (req, res) => {
 });
 
 app.get('/admin-portal', (req, res) => {
+  // TODO: render 3 arrays
+  const orderQuery = `SELECT `;
   res.render('adminPortal');
 });
 
@@ -186,7 +247,7 @@ app.get('/viewdb', (req, res) => {
   let query2 = 'select * from customer';
   mysql.pool.query(query2, (err, results) => {
     if (err) throw err;
-    console.log(results);
+    // console.log(results);
     res.send('database created successfully!');
   });
 });
--- public/js/managePost.js
+++ public/js/managePost.js
@@ -1,15 +1,33 @@
 window.addEventListener('DOMContentLoaded', function () {
   // Remember all of the existing twits in an array that we can use for search.
   const deleteBtns = document.getElementsByClassName('delete-btn');
+  const updateBtns = document.getElementsByClassName('update-btn');
 
+  // attach event listeners to delete buttons
   for (let i = 0; i < deleteBtns.length; i++) {
     deleteBtns[i].addEventListener('click', async event => {
-      const result = await axios.post('/manage-post', {
+      const result = await axios.post('/manage-post-delete', {
         postID: event.target.parentNode.parentNode.dataset.postid
       });
       if (result.data === 'OK') {
         event.target.parentNode.parentNode.remove();
       }
+    });
+  }
+
+  // attach event listeners to update buttons
+  for (let i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', async event => {
+      // grab quantity and price values
+      const result = await axios.post('/manage-post-update', {
+        postID: event.target.parentNode.parentNode.dataset.postid,
+        price: event.target.parentNode.parentNode.getElementsByTagName(
+          'input'
+        )[0].value,
+        quantity: event.target.parentNode.parentNode.getElementsByTagName(
+          'input'
+        )[1].value
+      });
     });
   }
 });
